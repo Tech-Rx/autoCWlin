c121db27b860d46d5671e236ca9c1854a39417a8
absaro <119333507+absaro@users.noreply.github.com>
2024-08-31 23:09:06 +0500

    Create credentials.sh

---
+++ credentials.sh
@@ -0,0 +1,23 @@
+# credentials.sh
+
+# GitHub Credentials
+export GITHUB_USERNAME="your_github_username"
+export GITHUB_PAT="your_github_pat"
+
+# Honeygain Credentials
+export HONEYGAIN_EMAIL="your_honeygain_email@example.com"
+export HONEYGAIN_PASS="your_honeygain_password"
+
+# Pawns Credentials
+export PAWNS_EMAIL="your_pawns_email@example.com"
+export PAWNS_PASS="your_pawns_password"
+
+# PacketStream Credentials
+export PACKETSTREAM_CID="your_packetstream_cid"
+
+# Repocket Credentials
+export REPOCKET_EMAIL="your_repocket_email@example.com"
+export REPOCKET_API_KEY="your_repocket_api_key"
+
+# ProxyRack Credentials
+export PROXYRACK_UUID="your_proxyrack_uuid"
